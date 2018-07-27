092180d5b8e0defcafc53b25b643133ee7aaba12
Pango-mac <cohena100@gmail.com>
2018-07-27 11:39:35 +0300

    finding in db

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect(
   'mongodb://localhost:27017/test/TodoApp',
@@ -23,19 +27,19 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection('Users').insertOne(
-      {
-        name: 'Avi',
-        age: 45,
-        location: 'Rehovot'
-      },
-      (err, result) => {
-        if (err) {
-          return console.log('Unable to insert user', err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-      }
-    );
+    // db.collection('Users').insertOne(
+    //   {
+    //     name: 'Avi',
+    //     age: 45,
+    //     location: 'Rehovot'
+    //   },
+    //   (err, result) => {
+    //     if (err) {
+    //       return console.log('Unable to insert user', err);
+    //     }
+    //     console.log(result.ops[0]._id.getTimestamp());
+    //   }
+    // );
 
     client.close();
   }
