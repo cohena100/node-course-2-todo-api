1c750835464986d110738d6b82361acef3dd4cd0
Avi <cohena100@gmail.com>
2018-08-02 01:57:15 +0300

    delete exercises

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -35,6 +35,20 @@ MongoClient.connect(
     //     console.log(result);
     //   });
 
+    // db
+    //   .collection('Users')
+    //   .deleteMany({ name: 'Avi' })
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    // db
+    //   .collection('Users')
+    //   .findOneAndDelete({ _id: new ObjectID('5b622d03f132cd8992a92c85')  })
+    //   .then(result => {
+    //     console.log(JSON.stringify(result, undefined, 2));
+    //   });
+
     // client.close();
   }
 );
