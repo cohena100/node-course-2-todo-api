551494a34fe424ae29236f718c3699a87e8057fd
Avi <cohena100@gmail.com>
2018-08-11 10:38:29 +0300

    added User model

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -28,13 +33,33 @@ var Todo = mongoose.model('Todo', {
 //   }
 // );
 
-var otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
-  completedAt: 123
+// var otherTodo = new Todo({
+//   text: 'Something to do'
+// });
+//
+// otherTodo.save().then(
+//   doc => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+//   },
+//   e => {
+//     console.log('Unable to save', err);
+//   }
+// );
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
 });
 
-otherTodo.save().then(
+var user = new User({
+  email: 'cohena100@gmail.com'
+});
+
+user.save().then(
   doc => {
     console.log(JSON.stringify(doc, undefined, 2));
   },
